fb0367019d5a9ff52005f679b86053004b19b6fc
kynefuk <namihira@heartbeats.jp>
2020-08-09 16:49:55 +0900

    add frontend

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    PermissionsMixin,
+)
+from django.contrib.auth.models import UserManager
+from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
+from django.contrib.auth.validators import UnicodeUsernameValidator
 
-# Create your models here.
+
+class CustomUserManager(UserManager):
+    pass
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    username_validator = UnicodeUsernameValidator()
+
+    username = models.CharField(
+        _("username"),
+        max_length=150,
+        unique=True,
+        help_text=_(
+            "Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only."
+        ),
+        validators=[username_validator],
+        error_messages={"unique": _("A user with that username already exists."),},
+    )
+    email = models.EmailField(_("email address"), blank=True)
+    is_staff = models.BooleanField(
+        _("staff status"),
+        default=False,
+        help_text=_("Designates whether the user can log into this admin site."),
+    )
+    is_active = models.BooleanField(
+        _("active"),
+        default=True,
+        help_text=_(
+            "Designates whether this user should be treated as active. "
+            "Unselect this instead of deleting accounts."
+        ),
+    )
+    date_joined = models.DateTimeField(_("date joined"), default=timezone.now)
+
+    objects = CustomUserManager()
+
+    EMAIL_FIELD = "email"
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = ["email"]
+
+    class Meta:
+        verbose_name = _("user")
+        verbose_name_plural = _("users")
