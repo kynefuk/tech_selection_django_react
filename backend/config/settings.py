ca5bc3eecd08e96af59e4742e59277210a6e2ad9
kynefuk <atsuminamihira@gmail.com>
2020-08-11 22:01:49 +0900

    add simple-jwt

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": str(BASE_DIR / "db.sqlite3"),
     }
 }
 
@@ -119,3 +119,10 @@ STATIC_URL = "/static/"
 
 # User
 AUTH_USER_MODEL = "accounts.CustomUser"
+
+# DRF
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    )
+}
